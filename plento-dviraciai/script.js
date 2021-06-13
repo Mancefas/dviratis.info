30bbd98c11d98f3831cab1aa678461dc4ff1e2b6
Mancefas <mantvydasliobe@gmail.com>
2021-06-13 21:29:00 +0300

    rėmo dydžio lentelė pakeista į skaičiuokle

--- plento-dviraciai/script.js
+++ plento-dviraciai/script.js
@@ -1,46 +1,40 @@
-//Pirmas aprasymas
-//const antJo = document.querySelector('.straipsniai-mgtk')
-//const ciaPridedam = document.querySelector('.aprasymas')
+'use strict';
 
-//antJo.addEventListener('mouseover', () => {
-//     ciaPridedam.classList.add('active');
-
-//})
-
-//const dingsta = document.querySelector('.aprasymas')
-
-//dingsta.addEventListener('mouseleave', () => {
-//     ciaPridedam.classList.remove('active')
-//})
-
-//Antras aprasymas
-
-//const antJo2 = document.querySelector('.straipsniai-mgtk2')
-//const ciaPridedam2 = document.querySelector('.aprasymas2')
-
-//antJo2.addEventListener('mouseover', () => {
- //    ciaPridedam2.classList.add('active');
-
-//})
-
-//const dingsta2 = document.querySelector('.aprasymas2')
-
-//dingsta2.addEventListener('mouseleave', () => {
- //    ciaPridedam2.classList.remove('active')
-//})
-
-const mygtukas = document.querySelectorAll(".straipsniai-mgtk")
+//Dviracio dydzio skaičiavimas pagal ūgį
 
 
-mygtukas.forEach( element => {
-     element.addEventListener('click', () => {
-          element.parentNode.classList.toggle('active');
-     })
+const skaiciuotiMygtukas = document.querySelector('.remo-skaiciavimas');
+const rekomenduojama = document.querySelector('.rekomendacija');
+
+skaiciuotiMygtukas.addEventListener('click', () => {
+     const ugis = Number(document.querySelector('.ugis').value);
+     if(ugis >= 148 && ugis <= 152){
+          rekomenduojama.textContent = 'Rekomenduojamas 47-48cm arba XXS dydis';
+          console.log('xxs')
+     } else if(ugis > 152 && ugis <= 160){
+          rekomenduojama.textContent = 'Rekomenduojamas 49-50cm arba XS dydis';
+          console.log('xs')
+     } else if(ugis > 160 && ugis <= 168){
+          rekomenduojama.textContent = 'Rekomenduojamas 51-53cm arba S dydis';
+          console.log('s')
+     } else if(ugis > 168 && ugis <= 175){
+          rekomenduojama.textContent = 'Rekomenduojamas 54-55cm arba M dydis';
+          console.log('m')
+     } else if(ugis > 175 && ugis <= 183){
+          rekomenduojama.textContent = 'Rekomenduojamas 56-58cm arba L dydis';
+          console.log('l')
+     } else if(ugis > 183 && ugis <= 191){
+          rekomenduojama.textContent = 'Rekomenduojamas <b>58-60cm</b> arba <b>XL</b> dydis';
+          console.log('xl')
+     } else if(ugis >191 && ugis <= 198){
+          rekomenduojama.textContent = 'Rekomenduojamas 61-63 arba XXL dydis';
+          console.log('xxl')
+     } else if(ugis > 198 && ugis < 215 ){
+          rekomenduojama.textContent = 'Rekomenduojama matuotis vietoje';
+     } else if(ugis <148 && ugis >125 ){
+          rekomenduojama.textContent = 'Rekomenduojama matuotis vietoje';
+     }
+     else {
+          rekomenduojama.textContent = 'Klaida';
+     }
 })
-
-
-
-
-
-
-
