552945e7a5a4fe5b46f767ee771e93a7d1448bfb
Mancefas <mantvydasliobe@gmail.com>
2021-06-14 12:39:40 +0300

    rėmo dydžio lentelė pakeista į skaičiuokle

--- plento-dviraciai/script.js
+++ plento-dviraciai/script.js
@@ -4,37 +4,39 @@
 
 
 const skaiciuotiMygtukas = document.querySelector('.remo-skaiciavimas');
-const rekomenduojama = document.querySelector('.rekomendacija');
+
+const rekomendacijosTekstas = (tekstas) => {
+     document.querySelector('.rekomendacija').textContent = tekstas;
+};
 
 skaiciuotiMygtukas.addEventListener('click', () => {
      const ugis = Number(document.querySelector('.ugis').value);
      if(ugis >= 148 && ugis <= 152){
-          rekomenduojama.textContent = 'Rekomenduojamas 47-48cm arba XXS dydis';
-          console.log('xxs')
+          rekomendacijosTekstas('Rekomenduojamas 47-48cm arba XXS dydis');
      } else if(ugis > 152 && ugis <= 160){
-          rekomenduojama.textContent = 'Rekomenduojamas 49-50cm arba XS dydis';
-          console.log('xs')
+          rekomendacijosTekstas('Rekomenduojamas 49-50cm arba XS dydis');
+          
      } else if(ugis > 160 && ugis <= 168){
-          rekomenduojama.textContent = 'Rekomenduojamas 51-53cm arba S dydis';
-          console.log('s')
+          rekomendacijosTekstas('Rekomenduojamas 51-53cm arba S dydis');
+          
      } else if(ugis > 168 && ugis <= 175){
-          rekomenduojama.textContent = 'Rekomenduojamas 54-55cm arba M dydis';
-          console.log('m')
+          rekomendacijosTekstas('Rekomenduojamas 54-55cm arba M dydis');
+          
      } else if(ugis > 175 && ugis <= 183){
-          rekomenduojama.textContent = 'Rekomenduojamas 56-58cm arba L dydis';
-          console.log('l')
+          rekomendacijosTekstas('Rekomenduojamas 56-58cm arba L dydis');
+          
      } else if(ugis > 183 && ugis <= 191){
-          rekomenduojama.textContent = 'Rekomenduojamas <b>58-60cm</b> arba <b>XL</b> dydis';
-          console.log('xl')
+          rekomendacijosTekstas('Rekomenduojamas 58-60cm arba XL dydis');
+          
      } else if(ugis >191 && ugis <= 198){
-          rekomenduojama.textContent = 'Rekomenduojamas 61-63 arba XXL dydis';
-          console.log('xxl')
+          rekomendacijosTekstas('Rekomenduojamas 61-63 arba XXL dydis');
+          
      } else if(ugis > 198 && ugis < 215 ){
-          rekomenduojama.textContent = 'Rekomenduojama matuotis vietoje';
+          rekomendacijosTekstas('Rekomenduojama matuotis vietoje');
      } else if(ugis <148 && ugis >125 ){
-          rekomenduojama.textContent = 'Rekomenduojama matuotis vietoje';
+          rekomendacijosTekstas('Rekomenduojama matuotis vietoje');
      }
      else {
-          rekomenduojama.textContent = 'Klaida';
+          rekomendacijosTekstas('Klaida');
      }
 })
